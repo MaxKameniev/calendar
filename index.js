aef3452f78f774632b187c9780a508ac2415e7c7
Max Kamenev <max@dwt.com.ua>
2019-01-18 13:36:59 +0200

    created UI

---
+++ index.js
@@ -0,0 +1,120 @@
+let root = document.querySelector('#root');
+
+let title = document.createElement('div');
+let titleName = document.createElement('div');
+let titleIcon = document.createElement('i');
+let calBody = document.createElement('div');
+let periodDate = document.createElement('div');
+let periodTitle = document.createElement('div');
+let moreIcon = document.createElement('i');
+let checkboxWrapper = document.createElement('div');
+let timezone = document.createElement('div');
+let timeZoneTitle = document.createElement('div');
+let titmezoneMoreIcon = document.createElement('i');
+let blockFromTo = document.createElement('div');
+let caseFrom = document.createElement('div');
+let caseTo = document.createElement('div');
+let titleFrom = document.createElement('span');
+let titleTo = document.createElement('span');
+let dateFrom = document.createElement('div');
+let dateTo = document.createElement('div');
+let dateFromTitle = document.createElement('div');
+let dateToTitle = document.createElement('div');
+let dateFromIcon = document.createElement('i');
+let dateToIcon = document.createElement('i');
+let buttonsBlock = document.createElement('div');
+let buttonCancel = document.createElement('button');
+let buttonApply = document.createElement('button');
+
+
+let isPeriodOpen = false;
+let periodArr = ['Custom', 'Today', 'Yesterday', 'Last 7 days', 'Last 30 days', 'Last 90 days'];
+
+
+moreIcon.addEventListener('click', showPeriod);
+
+title.classList.add('calendar__title');
+titleName.classList.add('title__name');
+titleIcon.className = "title__icon fas fa-calendar-alt";
+calBody.classList.add('calendar__body');
+periodDate.classList.add('period__date');
+periodTitle.classList.add('title__name');
+moreIcon.className = "title__icon fas fa-chevron-down";
+checkboxWrapper.className = "period__checkbox checkbox--hidden";
+timezone.classList.add('timezone__title');
+timeZoneTitle.classList.add('title__name');
+titmezoneMoreIcon.className = "title__icon fas fa-chevron-down";
+blockFromTo.classList.add('datechoose__block');
+caseFrom.classList.add('casedate__block');
+caseTo.classList.add('casedate__block');
+dateFrom.classList.add('date__block');
+dateTo.classList.add('date__block');
+dateFromTitle.classList.add('title__name');
+dateToTitle.classList.add('title__name');
+dateFromIcon.className = "title__icon fas fa-calendar-alt";
+dateToIcon.className = "title__icon fas fa-calendar-alt";
+buttonsBlock.classList.add('buttons_block');
+buttonCancel.classList.add('button');
+buttonApply.classList.add('button');
+
+
+titleName.textContent = "Date";
+periodTitle.textContent = "Period";
+timeZoneTitle.textContent = "Timezone";
+titleFrom.textContent = "From";
+titleTo.textContent = "To";
+dateFromTitle.textContent = "DateFrom";
+dateToTitle.textContent = "DateTo";
+buttonCancel.textContent = "Cancel";
+buttonApply.textContent = "Apply";
+
+
+root.append(title);
+title.append(titleName);
+title.append(titleIcon);
+root.append(calBody);
+calBody.append(periodDate);
+periodDate.append(periodTitle);
+periodDate.append(moreIcon);
+calBody.append(checkboxWrapper);
+calBody.append(timezone);
+timezone.append(timeZoneTitle);
+timezone.append(titmezoneMoreIcon);
+calBody.append(blockFromTo);
+blockFromTo.append(caseFrom);
+blockFromTo.append(caseTo);
+caseFrom.append(titleFrom);
+caseTo.append(titleTo);
+caseFrom.append(dateFrom);
+caseTo.append(dateTo);
+dateFrom.append(dateFromTitle);
+dateTo.append(dateToTitle);
+dateFrom.append(dateFromIcon);
+dateTo.append(dateToIcon);
+calBody.append(buttonsBlock);
+buttonsBlock.append(buttonCancel);
+buttonsBlock.append(buttonApply);
+
+
+function showPeriod() {
+    isPeriodOpen = !isPeriodOpen;
+    checkboxWrapper.className = isPeriodOpen ? "period__checkbox checkbox--showed" : "period__checkbox checkbox--hidden";
+};
+
+
+for(let i of periodArr){
+    let checkItem = document.createElement('div');
+    let inputItem = document.createElement('input');
+    let labelItem = document.createElement('label');
+    checkboxWrapper.append(checkItem);
+    checkItem.append(labelItem);
+    checkItem.append(inputItem);
+    inputItem.setAttribute('type', 'checkbox');
+    inputItem.setAttribute('value', i);
+    labelItem.setAttribute('for', i);
+    labelItem.textContent = i;
+}
+
+
+let timeZone = new Date().getTimezoneOffset();
+console.log(timeZone);
